3f76634acf531f66b21375404c2e88427f802721
Huynh <phamtranhuynh92@gmail.com>
2016-06-18 08:59:42 +0000

    init

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // # SimpleServer
 // A simple chat bot server
 
-var logger = require('morgan');
 var http = require('http');
+var log = require('simple-node-logger').createSimpleLogger('chatbox.log');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 var express = require('express');
 var request = require('request');
@@ -22,11 +23,54 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+  if (req.query['hub.verify_token'] === 'huynhit92') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 });
+
+// Xử lý khi có người nhắn tin cho bot
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    log.info(entry);
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        console.log(message.message);
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
+        }
+      }
+    }
+  }
+
+  res.status(200).send("OK");
+});
+
+
+// Gửi thông tin tới REST API để trả lời
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "EAAJJkzXXmlEBAOlhtRxVP491ybt3p5bVL2wTMpBu2HTpRYwgU8s74RF4rZCbOsSYSNvkcdmuhcU0iPD8SXcqbZCDbREFRrOeeTeIqntuLNPpRhiebcGD6Rzmjogn2q3ZBj4deaCFqX9fcdPvTsIOmXIJp6l0cLfqus8W9etxAZDZD",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
